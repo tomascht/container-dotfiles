1b7cb4f8a43efc3cdae67abf72b164c223973595
Tim Tomaschoff <tim.tomaschoff@berge-meer.de>
2025-12-18 10:10:49 +0100

    initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing dotfiles and more"
+apt update
+apt upgrade
+
+echo "Installing zsh"
+apt install zsh
+
+touch .profile
+echo "export SHELL=`which zsh`" >> .profile
+echo "`[ -z "$ZSH_VERSION" ] && exec \"$SHELL\" -l"
