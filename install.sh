1d1436ca06598a38e30e30568c65450abf348d5c
Tim Tomaschoff <tim.tomaschoff@berge-meer.de>
2025-12-18 13:03:02 +0100

    wip

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Installing dotfiles and more"
-#sudo apt update
-#sudo apt upgrade
-
-#echo "Installing zsh"
-#sudo apt install zsh
-
-#touch ~/.profile
 echo "install brew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -21,6 +14,7 @@ brew install zsh vim neovim ripgrep
 echo 'exec zsh' >>~/.bashrc
 
 # Lazyvim
-mv ~/.config/nvim{,.bak}
-
+# mv ~/.config/nvim{,.bak}
 git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
