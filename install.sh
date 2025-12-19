55cf0985e497285733c747bafabb75d47a3dc9ab
Tim Tomaschoff <tim.tomaschoff@berge-meer.de>
2025-12-19 14:53:30 +0100

    mise setup

--- install.sh
+++ install.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
 echo "Installing dotfiles and more"
-echo "install brew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "install mise"
+sudo apt update -y && sudo apt install -y curl
+sudo install -dm 755 /etc/apt/keyrings
+curl -fSs https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.pub 1>/dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.pub arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise ripgrep lazygit
 
+# clear .bashrc
 echo >>~/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.bashrc
 
+# use zsh
+echo 'exec zsh' >>~/.bashrc
+
+# active mise
+echo 'eval "$(~/.local/bin/mise activate zsh)"' >>~/.zshrc
+# reload .bashrc
 source ~/.bashrc
 
-brew install zsh neovim ripgrep lazygit zoxide
-
-echo 'exec zsh' >>~/.bashrc
+# Neovim
+mise plugins add neovim
+mise i neovim@nightly
 
 # Lazyvim
 # mv ~/.config/nvim{,.bak}
